daef5bf2a322ec968c84d7b6b67c6053b081bdb6
Michael Marino <mmarino@gmail.com>
2014-10-10 17:06:04 +0200

    Add support for aggregate database
    Help address nEDM-TUM/nEDM-Interface#12

--- couchdb_updater.py
+++ couchdb_updater.py
@@ -3,11 +3,14 @@ import sys
 import json
 import re
 import logging
+from datetime import datetime as _dt
+import threading as _th
 
 # global variables
-_ignored_dbs = []
+_ignored_dbs = [ "nedm%2Faggregate" ]
 _handled_dbs = {}
 _max_updates = 100
+_tracked_types = ["heartbeat", "data"]
 
 # Give us some logging
 logging.basicConfig(filename='/path/to/update_indexer.log', level=logging.INFO)
@@ -42,6 +45,24 @@ def update_views(db_name, ddocs):
         adb.design(des_doc).view(view).get(
             params=dict(limit=1,stale="update_after"))
 
+def _post_doc_to_aggregate(doc):
+    """
+    post to the aggregate db and block for the result
+    """
+    db = _acct["nedm%2Faggregate"]
+    return db.design("aggregate").put("_update/aggregate/" + doc["id"], params=doc).result()
+
+def get_most_recent_docs(db_name):
+    db = _acct[db_name]
+    rec_list = [(t, db.design('document_type').view('document_type').get(
+                   params=dict(limit=1,reduce=False,endkey=[t], startkey=[t,{}], descending=True)).result().json()['rows'])
+                 for t in _tracked_types ]
+    for t, r in rec_list:
+        if len(r) == 0: continue
+        doc = dict(id=db_name + ":" + t, refid=r[0]['id'],
+               timestamp=_dt(*r[0]["key"][1:]).strftime("%a, %d %b %Y %H:%M:%S GMT"))
+        _post_doc_to_aggregate(doc)
+
 def listen(reg_exp):
     """
       Listen to stdin, and update databases that match the reg expression
@@ -66,7 +87,8 @@ def listen(reg_exp):
                 continue
             _handled_dbs[db_name] = {
                                       'views' : get_views_for_db(db_name),
-                                      'counter' : 0
+                                      'counter' : 0,
+                                      'update_docs' : None
                                     }
             logging.info("Tracking: " + db_name)
 
@@ -84,6 +106,11 @@ def listen(reg_exp):
         if the_db['counter'] >= _max_updates:
             update_views(db_name, the_db['views'])
             the_db['counter'] = 0
+
+        # Update aggregate db, ignore if currently running
+        if not the_db['update_docs'] or not the_db['update_docs'].is_alive():
+            the_db['update_docs'] = _th.Thread(target=get_most_recent_docs, args=(db_name,))
+            the_db['update_docs'].start()
 
 if __name__ == '__main__':
     logging.info(" ######### UPDATER starting ########## ")
