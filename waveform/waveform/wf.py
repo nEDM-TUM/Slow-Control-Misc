808fa93e8f1bc22cfa002c149a4b327325cf9d74
flotum <flotum@users.noreply.github.com>
2016-06-05 12:24:20 +0200

    Update wf.py

--- waveform/waveform/wf.py
+++ waveform/waveform/wf.py
@@ -7,8 +7,8 @@ def makeArrayToSend(b0, F_Burst_Time, Sampling_Freq, Burst_Width, Measuring_Time
 	 
     b0_field_strength = b0 # muT
 
-    gamma_xe = 11.77717 # (10^6 rad*s^-1*T^-1)/(2Pi)=MHz/T
-    gamma_he = 32.4341  # 
+    gamma_xe = 11.77673923 # (10^6 rad*s^-1*T^-1)/(2Pi)=MHz/T - REF: M. Pfeffer, J. Magn. Res., Vol 108, 1994
+    gamma_he = 32.4341008  #  REF Flowers, Metrologia, Vol 30, 1993
     
     volt_per_muT = 17.29                                 ###volt_per_muT = 2*17.29 # V_pp / muT (Rafael)
     
